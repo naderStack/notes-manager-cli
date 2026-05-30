ec5b3b63d12925239147f36e0d27f01442405468
naderStack <wa21nadirhi@gmail.com>
2026-05-30 21:32:41 +0200

    add feature filter by tags

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-let command = process.argv[2]
-let note = process.argv[3]
+let command = process.argv[2] // [ add , delete , list ]
+let note = process.argv[3]  // [value ]
+let tag = process.argv[4] // [--tag]
+let value_tag = process.argv[5]  // [ value tag]
+
 
 // check if user type commands or not 
-// if (!command || !note){
-//     console.log('nothing')
-//     process.exit(1)
-// }
+if (!command ){
+    console.log('nothing')
+    process.exit(1)
+}
 
+// add -t 'nader' 
 
 //init database
 import path, { dirname, join } from 'path'
 import url, { fileURLToPath } from 'url'
-
+// [debug ]
 // console.log(import.meta.url) // file:///home/nader/Desktop/cli-notes-taker/src/index.js
 // console.log(fileURLToPath(import.meta.url))     // /home/nader/Desktop/cli-notes-taker/src/index.js
 
@@ -24,46 +28,74 @@ import Table from 'cli-table3'
 
 let db = new sqlite3.Database(join(__dirname,'database.db'))
 
- db.exec(`create table if not exists notes (id  integer primary key autoincrement , name text )`)
+ db.exec(`create table if not exists notes (id  integer primary key autoincrement , name text , tags text , dateAt  timestamp default current_timestamp )`)
 
 switch (command) {
     case 'add':
-
-            db.run(`insert into notes (name) values (? )`, note)
-
-            console.log(`done added  : ${note}`)
+    // add a Note when typing <name Note > and < tag Name> [require]
             
-            
+    if (note && tag=='--tag' && value_tag) {
+
+        value_tag  = value_tag.split(",")
+     
+
+        db.run(`insert into notes (name , tags ) values (?,?)`, [ note ,  JSON.stringify( value_tag ) ]  )
+        
+    } else {
+        console.log("type a tag for your note ")  // warning when add a note without tagname 
+    }                   
+
         break;
 
 case 'list':
-    db.all('select * from notes', [], (err, data) => {
-        if (err) {
-            console.error(err);
-            return;
+        tag = process.argv[3]
+
+        value_tag = process.argv[4]
+
+            // show all Notes 
+        if (!tag){
+
+            db.all('select * from notes', [], (err, data) => {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
+                    
+                    const table = new Table({
+                        head: ['ID', 'Name' , 'tags','create at'],
+                        colWidths: [5, 30,20,30]
+                    });
+                    
+                    data.forEach(row => {
+                        table.push([row.id, row.name , row.tags , row.dateAt]);
+                    });
+                    
+                    console.log(table.toString());
+                })
+
+
+        } 
+
+        // show a note when type --tag <tagname> if it match [a note with tag ]
+        if (tag == "--tag" && value_tag){
+          db.all(`select * from notes where tags like ?`,[`%${value_tag}%`], (err,row)=>{
+
+
+             const tableTag = new Table({
+                        head: ['ID', 'Name' , 'tags','create at'],
+                        colWidths: [5, 30,20,30]
+                    });
+                    
+                    row.forEach(r => {
+                        tableTag.push([r.id, r.name , r.tags , r.dateAt]);
+                    });
+            
+    
+                    console.log(tableTag.toString());
+          })
+
         }
-        
-        const table = new Table({
-            head: ['ID', 'Name'],
-            colWidths: [5, 50]
-        });
-        
-        data.forEach(row => {
-            table.push([row.id, row.name]);
-        });
-        
-        console.log(table.toString());
-    })
     break;
-// case 'list':
-//     db.all('select * from notes', [], (err, data) => {
-//         if (err) {
-//             console.error(err);
-//             return;
-//         }
-//         console.table(data);
-//     })
-//     break;
 
    case 'delete':
            if (note){
